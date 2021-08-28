4cd10d30a26a47f45c0eb3753adc183e3810e47b
Mazda Shahzadi <s3841929@student.rmit.edu.au>
2021-08-28 11:37:36 +1000

    Added simple book container

--- FrontEnd/myfirstapp/src/components/Market_Place/BookContainer.js
+++ FrontEnd/myfirstapp/src/components/Market_Place/BookContainer.js
@@ -1,11 +1,59 @@
-import './styles.css';
-import React from "react";
-import { Paper } from '@material-ui/core';
+//import './styles.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-const BookContainer = () => {
-    return (<div>
-            <Paper>Hello</Paper>
-    </div>);
+import img from "./BookImages/DefaultCover.png";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 275,
+  },
+  media: {
+    height: 140,
+    //max-width: 25%,
+    //maxWidth: 25%
+    
+  },
+});
+
+export default function BookContainer() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          
+          image= {img}
+          title="Default book cover"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="h2">
+            Book1
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Book text
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Buy
+        </Button>
+        <Button size="small" color="primary">
+          Book details
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
  
-export default BookContainer;
+//export default BookContainer;
--- FrontEnd/myfirstapp/src/components/Market_Place/Marketplace.js
+++ FrontEnd/myfirstapp/src/components/Market_Place/Marketplace.js
@@ -1,21 +1,27 @@
-import { Grid } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 import React from 'react';
 import BookContainer from './BookContainer';
 import './styles.css';
 
 const Marketplace = (props) => {
     return ( <div>
+    <Box p={5} b= {5}>
         <Grid container 
-        direction="row"
-        justifyContent="space-between"
-        alignItems="center"
-        spacing={2}>
-
-            <Grid item sm={6} md={4} xs={12}><BookContainer /></Grid>
-            <Grid item sm={6} md={4} xs={12}><BookContainer /></Grid>
-            <Grid item sm={6} md={4} xs={12}><BookContainer /></Grid>
+        //direction="row"
+        //justifyContent="space-between"
+        //alignItems="center"
+        b= {5}
+        spacing={5}>
+            
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
+            <Grid item sm={6} md={3} xs={12} spacing ={4}><BookContainer /></Grid>
             
         </Grid>
+        </Box>    
     </div> );
 }
  
