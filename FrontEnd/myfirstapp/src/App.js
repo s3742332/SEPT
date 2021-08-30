ff8fc1613197b366317b08d53f4e6f9a98a9cb6a
s3719046 <37074069+s3719046@users.noreply.github.com>
2021-08-30 17:52:13 +1000

    Add new frontend code from Homy

--- FrontEnd/myfirstapp/src/App.js
+++ FrontEnd/myfirstapp/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
@@ -7,15 +7,38 @@ import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-d
 import AddPerson from "./components/Persons/AddPerson";
 import { Provider } from "react-redux";
 import store from "./store";
-
+import { logout } from "./actions/securityActions";
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
 import PendingSeller from "./components/AccountManagement/PendingSeller/PendingSeller";
 import AccountEdit from "./components/AccountManagement/AccountEdit/AccountEdit";
+import SecuredRoute from "./securityUtils/SecureRoute";
+import jwt_decode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+
 
 function App() {
   const isAdmin = true;
+  const jwtToken = localStorage.jwtToken;
+
+  useEffect(() => {
+    if (jwtToken) {
+      setJWTToken(jwtToken);
+      const decoded_jwtToken = jwt_decode(jwtToken);
+      store.dispatch({
+        type: SET_CURRENT_USER,
+        payload: decoded_jwtToken
+      });
+    
+      const currentTime = Date.now() / 1000;
+      if (decoded_jwtToken.exp < currentTime) {
+        store.dispatch(logout());
+        window.location.href = "/";
+      }
+    }
+  }, [])
   return (
     <Provider store={store}>
       <Router>
--- FrontEnd/myfirstapp/src/actions/securityActions.js
+++ FrontEnd/myfirstapp/src/actions/securityActions.js
@@ -1,30 +1,30 @@
 import axios from "axios";
-import {GET_ERRORS, SET_CURRENT_USER} from "./types";
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 import setJWTToken from "../securityUtils/setJWTToken";
 import jwt_decode from "jwt-decode";
 
 
 export const createNewUser = (newUser, history) => async dispatch => {
 
-    try{
+    try {
         console.log("123123123", newUser);
         const config = {
             headers: {
                 "Content-Type": "application/json",
                 "Access-Control-Allow-Origin": "*"
-              }
+            }
         }
-        await axios.post("http://localhost:8080/api/users/register", newUser, config).then((response)=> {
+        await axios.post("http://localhost:8080/api/users/register", newUser, config).then((response) => {
             console.log(response)
-    });
-        
+        });
+
         dispatch({
             type: GET_ERRORS,
             payload: {}
         });
     }
-    catch (err){
-        dispatch ({
+    catch (err) {
+        dispatch({
             type: GET_ERRORS,
             payload: err.response.data
         });
@@ -35,48 +35,81 @@ export const createNewUser = (newUser, history) => async dispatch => {
 
 };
 
+// export const login = LoginRequest => async dispatch => {
+//     try{
+//         const config = {
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 "Access-Control-Allow-Origin": "*"
+//               }
+//         }
+//         await axios.post("http://localhost:8080/api/users/login", LoginRequest, config);
+
+//         dispatch({
+//             type: GET_ERRORS,
+//             payload: {}
+//         });
+//     }
+//     catch (err){
+//         dispatch ({
+//             type: GET_ERRORS,
+//             payload: err.response.data
+//         });
+
+
+
+//     }
+// try {
+
+//     //post => login request
+
+//     //extract token from res.data
+
+//     //set our token in the local storage
+
+//     // set our token in header 
+
+//     //decode the token on React
+
+//     // dispatch to our securityReducer
+
+// }
+// catch (err)
+// {
+
+// }
+
+//}
 export const login = LoginRequest => async dispatch => {
-    try{
-        const config = {
-            headers: {
-                "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*"
-              }
-        }
-        await axios.post("http://localhost:8080/api/users/login", LoginRequest, config);
-        
+    try {
+        // post => Login Request
+        const res = await axios.post("/api/users/login", LoginRequest);
+        // extract token from res.data
+        const { token } = res.data;
+        // store the token in the localStorage
+        localStorage.setItem("jwtToken", token);
+        // set our token in header ***
+        setJWTToken(token);
+        // decode token on React
+        const decoded = jwt_decode(token);
+        // dispatch to our securityReducer
         dispatch({
-            type: GET_ERRORS,
-            payload: {}
+            type: SET_CURRENT_USER,
+            payload: decoded
         });
-    }
-    catch (err){
-        dispatch ({
+    } catch (err) {
+        dispatch({
             type: GET_ERRORS,
             payload: err.response.data
         });
-
-
-
     }
-    // try {
+};
 
-    //     //post => login request
-
-    //     //extract token from res.data
-
-    //     //set our token in the local storage
-
-    //     // set our token in header 
-
-    //     //decode the token on React
-
-    //     // dispatch to our securityReducer
-
-    // }
-    // catch (err)
-    // {
-
-    // }
-
-}
+export const logout = () => dispatch => {
+    localStorage.removeItem("jwtToken");
+    setJWTToken(false);
+    dispatch({
+        type: SET_CURRENT_USER,
+        payload: {}
+    });
+};
