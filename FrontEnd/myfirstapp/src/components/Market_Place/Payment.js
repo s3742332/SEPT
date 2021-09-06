dcac4a5f5fecedb517b8893a569779d18bf10acc
s3742332 <s3742332@student.rmit.edu.au>
2021-09-06 21:29:38 +1000

    added states

--- FrontEnd/myfirstapp/src/components/Market_Place/Payment.js
+++ FrontEnd/myfirstapp/src/components/Market_Place/Payment.js
@@ -2,9 +2,44 @@
 import React from 'react';
 import img from "./BookImages/DefaultCover.png";
 import { Button, Card, Input, DatePicker } from 'antd';
+import { useState } from 'react';
 
 
 export default function Payment(props) {
+
+    //useStates
+    const [fName, setFName] = useState('');
+    const [LName, setLName] = useState('');
+    const [email, setEmail] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [expDate, setExpDate] = useState('');
+    const [address, setAddress] = useState('');
+
+    const handleFirstName = (event) => {
+        event.preventDefault();
+        setFName(event.target.value);
+    };
+    const handleLastName = (event) => {
+        event.preventDefault();
+        setLName(event.target.value);
+    };
+    const handleEmail = (event) => {
+        event.preventDefault();
+        setEmail(event.target.value);
+    };
+    const handleCardNumber = (event) => {
+        event.preventDefault();
+        setCardNumber(event.target.value);
+    };
+    const handleExpDate = (event) => {
+        event.preventDefault();
+        setExpDate(event.target.value);
+    };
+    const handleAddress = (event) => {
+        event.preventDefault();
+        setAddress(event.target.value);
+    };
+    
     return (
         <Card style={{ width: 'fit-content', position:'relative', left:'50%', transform:'translate(-50%)', padding:'1%', marginTop:'5%' }}>
             <div >
@@ -16,7 +51,7 @@ export default function Payment(props) {
                 Email <br />
                 <Input type="email" style={{ width:'300px', marginBottom: '3%' }}></Input><br />
                 Card Number <br />
-                <Input type='number' style={{ width:'300px', marginBottom: '3%' }}></Input><br />
+                <Input type='number' pattern="[0-9\s]" style={{ width:'300px', marginBottom: '3%' }}></Input><br />
                 <Input.Group compact>
                     Expiry Date <br />
                     <DatePicker.RangePicker style={{ width:'300px', marginBottom: '3%' }} />
