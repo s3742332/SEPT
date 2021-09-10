35324d7002c7720df6223e997eaec8ae7278035a
Tessa Podbury <65106860+tepo-design@users.noreply.github.com>
2021-09-10 21:16:43 +1000

    added endpoints for pending approved books

--- BackEnd/booksmicroservices/src/main/java/com/rmit/sept/bk_booksmicroservices/web/BookController.java
+++ BackEnd/booksmicroservices/src/main/java/com/rmit/sept/bk_booksmicroservices/web/BookController.java
@@ -1,13 +1,14 @@
 package com.rmit.sept.bk_booksmicroservices.web;
 
-import javax.validation.Valid;
-
 import com.rmit.sept.bk_booksmicroservices.Services.BookService;
 import com.rmit.sept.bk_booksmicroservices.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/books")
@@ -30,6 +31,34 @@ public class BookController {
         Iterable<Book> bookList = bookService.getAllBooks();
 
         return  new ResponseEntity<Iterable<Book>>(bookList, HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/getAllPendingBooks")
+    public ResponseEntity<?> getAllPendingBooks(){
+        Iterable<Book> bookList = bookService.getAllBooks();
+        ArrayList<Book> unapprovedBooks = new ArrayList<Book>();
+        for (Book book : bookList){
+            if (book.getApproved().equals(false)) {
+                unapprovedBooks.add(book);
+            }
+        }
+
+        return  new ResponseEntity<Iterable<Book>>(unapprovedBooks, HttpStatus.OK);
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/getAllApprovedBooks")
+    public ResponseEntity<?> getAllApprovedBooks(){
+        Iterable<Book> bookList = bookService.getAllBooks();
+        ArrayList<Book> approvedBooks = new ArrayList<Book>();
+        for (Book book : bookList){
+            if (book.getApproved().equals(true)) {
+                approvedBooks.add(book);
+            }
+        }
+
+        return  new ResponseEntity<Iterable<Book>>(approvedBooks, HttpStatus.OK);
     }
 
 
