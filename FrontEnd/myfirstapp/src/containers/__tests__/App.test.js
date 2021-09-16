5010243ac470767dd43512d9e9dbf339ac1b1d59
3840992 <s3840992@student.rmit.edu.au>
2021-09-16 23:08:02 +1000

    Update test case to use test function

--- FrontEnd/myfirstapp/src/containers/__tests__/App.test.js
+++ FrontEnd/myfirstapp/src/containers/__tests__/App.test.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import store from "../../store";
     
 describe('App', () => {
-  it('renders without crashing given the required props', () => {
+  test('renders without crashing given the required props', () => {
     const props = {
       isFetching: false,
       dispatch: jest.fn(),
