9e765086d532015b3f6521d2cb3fce1d83fcf4a0
s3742332 <s3742332@student.rmit.edu.au>
2021-09-17 22:27:44 +1000

    Update Sell.js

--- FrontEnd/myfirstapp/src/components/Sell/Sell.js
+++ FrontEnd/myfirstapp/src/components/Sell/Sell.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react'
+import { bookEdit } from '../../actions/bookActions';
 import {
     Form,
     Input,
@@ -15,38 +17,66 @@ import {
     Typography
 } from 'antd';
 import { UploadOutlined, PlusOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux'
+
 function Sell() {
+    const dispatch = useDispatch();
     const { Title } = Typography
     const { Option } = Select;
     const { TextArea } = Input;
+    const security = useSelector(state => state.security);
+    const [newBook, setNewBook] = useState({
+        seller: security.user.fullName,
+        bookTitle: null,
+        author: null,
+        bookDescription: null,
+        bookCost: null,
+        stockLevel: null,
+        approved: false,
+        isbn: null,
+        cover: null,
+        category: null
+    });
     function handleChange(value) {
         console.log(`Selected: ${value}`);
     }
-    const children = [];
-    for (let i = 10; i < 36; i++) {
-        children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
-    }
+    const children = [<Option key='fantasy'>fantasy</Option>, <Option key='adventure'>adventure</Option>, <Option key='thriller'>thriller</Option>, <Option key='romance'>romance</Option>, <Option key='contemporary'>contemporary</Option>, <Option key='distopian'>distopian</Option>, <Option key='mystery'>mystery</Option>, <Option key='horror'>horror</Option>];
+
     const [form] = Form.useForm();
 
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(bookEdit(newBook));
+    }
+
+    const handleChanges = (event) => {
+        event.preventDefault();
+        setNewBook({ ...newBook, [event.target.name]: event.target.value });
+        console.log(newBook)
+    }
+
+
     return (
-        <Row justify="center" style={{height: "100%"}}>
+        <Row justify="center" style={{ height: "100%" }}>
             <Col>
                 <Title>Sell your Book</Title>
                 <Form
                     layout="horizontal"
-                    labelCol={{ span: 12}}
+                    labelCol={{ span: 12 }}
                     wrapperCol={{ span: 14 }}
                     form={form}
                     size="medium"
+                    onChange={handleChanges}
                 >
                     <Form.Item label="Name">
-                        <Input />
+                        <Input name='bookTitle' />
                     </Form.Item>
                     <Form.Item label="Author">
-                        <Input />
+                        <Input name="author" />
                     </Form.Item>
                     <Form.Item label="Description">
-                        <TextArea />
+                        <TextArea name="bookDescription" />
                     </Form.Item>
 
                     <Form.Item label="Genres">
@@ -56,25 +86,27 @@ function Sell() {
                             placeholder="Please select"
                             onChange={handleChange}
                             style={{ width: '100%' }}
+                            name="cetegories"
                         >
                             {children}
                         </Select>
                     </Form.Item>
                     <Form.Item label="Book Cover">
-                        <Upload >
-                            <Button icon={<UploadOutlined />}>Upload</Button>
-                        </Upload>
+                        <Input name="cover" />
                     </Form.Item>
                     <Form.Item label="Book PDF (Preview)">
                         <Upload >
                             <Button icon={<UploadOutlined />}>Upload</Button>
                         </Upload>
                     </Form.Item>
-                    <Form.Item label="Cost">
-                        <InputNumber />
+                    <Form.Item label="ISBN">
+                        <InputNumber name="isbn" />
                     </Form.Item>
-                    <Form.Item style={{justifyContent: "center", textAlign: "center"}}>
-                        <Button>Add</Button>
+                    <Form.Item label="Cost">
+                        <InputNumber name="price" />
+                    </Form.Item>
+                    <Form.Item style={{ justifyContent: "center", textAlign: "center" }}>
+                        <Button type='submit' onClick={onSubmit}>Add</Button>
                     </Form.Item>
 
                 </Form>
