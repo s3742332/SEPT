045b875d1a830b30e02f979295659cd8317abb00
s3742332 <s3742332@student.rmit.edu.au>
2021-10-03 21:38:32 +1100

    error is thrown when user is banned

--- BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/exceptions/CustomResponseEntityExceptionHandler.java
+++ BackEnd/loginmicroservices/src/main/java/com/rmit/sept/bk_loginservices/exceptions/CustomResponseEntityExceptionHandler.java
@@ -11,12 +11,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 @RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
-
-
-
     @ExceptionHandler
     public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request){
         UsernameAlreadyExistsResponse exceptionResponse = new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUserIsBanned(UserIsBannedException ex, WebRequest request){
+        UserIsBannedResponse exceptionResponse = new UserIsBannedResponse(ex.getMessage());
         return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
