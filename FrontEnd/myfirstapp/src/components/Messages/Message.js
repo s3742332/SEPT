ab59acd68ef3eeec7d0188a989e6f9c675009789
s3742332 <s3742332@student.rmit.edu.au>
2021-10-09 16:48:03 +1100

    added front end styling

--- FrontEnd/myfirstapp/src/components/Messages/Message.js
+++ FrontEnd/myfirstapp/src/components/Messages/Message.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Card, Input } from 'antd';
+import { Alert, Button, Card, Input } from 'antd';
 import TextArea from "rc-textarea";
 import { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,6 +8,7 @@ import { saveMessage } from "../../actions/messageActions";
 const Message = () => {
     const [text, setText] = useState('')
     const [sent, setSent] = useState(false)
+    const [alert, setAlert] = useState(false)
     const security = useSelector(state => state.security);
     const dispatch = useDispatch();
 
@@ -18,13 +19,18 @@ const Message = () => {
 
     const submitMessage = (e) => {
         e.preventDefault();
-        let message = {
-            username: security.user.fullName,
-            message: text
+        if (text.length < 255 && text.length > 5) {
+            let message = {
+                username: security.user.fullName,
+                message: text
+            }
+            dispatch(saveMessage(message));
+            
+            setAlert(false)
+            setSent(true);
+        } else {
+            setAlert(true)
         }
-        dispatch(saveMessage(message));
-
-        setSent(true);
     }
 
     return (
@@ -35,6 +41,7 @@ const Message = () => {
                         <h1>Send a Message to our Admins</h1>
                         <TextArea rows={10} placeholder="Contact Our Admins" style={{ width: " 45% " }} onChange={handleText} />
                         <Button type="primary" shape="round" size="large" style={{ marginTop: 20 }} onClick={submitMessage} >Submit</Button>
+                        {!alert ? '' : <Alert message="Message must be between 5 and 255 Characters" type="error" style={{marginTop: 20}} />}
                     </div>
                     :
                     <div style={{ display: "grid", placeItems: "center", paddingTop: 100 }}>
