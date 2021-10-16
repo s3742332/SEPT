40c0240b00324b59180292e6573b13640824ab09
s3742332 <s3742332@student.rmit.edu.au>
2021-10-16 19:23:54 +1100

    added links to seller page

--- FrontEnd/myfirstapp/src/components/Seller/Seller.js
+++ FrontEnd/myfirstapp/src/components/Seller/Seller.js
@@ -1,14 +1,48 @@
 import { Card, List } from "antd";
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { getUserAccountsList } from "../../actions/userActions";
+import { useLocation } from "react-router";
 
 export default (props) => {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.user);
+    const location = useLocation()
+    const [loading, setLoading] = useState(true);
+    const [userList, setUserList] = useState([]);
+    const [account, setAccount] = useState({});
+    const {seller} = location.state;
+
+
+    useEffect(() => {
+        console.log('seller',seller)
+        dispatch(getUserAccountsList())
+        setLoading(false)
+    }, [dispatch])
+    useEffect(() => {
+        if (!loading) {
+            setUserList(user.userAccounts);
+            console.log('userlist',userList)
+        }
+    }, [loading])
+    useEffect(() => {
+        if (userList) {
+            userList.forEach(user => {
+                if (user.email == seller) {
+                    setAccount(user);
+                    console.log('acc',account)
+                }
+            });
+        }
+    }, [])
     return (
-        <div style={{display:'flex'}}>
+        !account ? 'LOADING' :
+        <div style={{ display: 'flex' }}>
             <Card style={{ width: '30%', margin: '5%' }}>
                 <h1>SELLER</h1>
                 <br /><br />
                 <h5>PENGUIN BOOKS</h5>
-                <h5>EMAIL@EMAIL.com</h5>
+                <h5>{account.email}</h5>
                 <h5>0123456789</h5>
                 <h5>0987654321123456</h5>
             </Card>
