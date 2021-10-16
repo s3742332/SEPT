6d6f950abfe10fff00f304f7f77e0b9e836aa4e2
s3742332 <s3742332@student.rmit.edu.au>
2021-10-16 20:04:35 +1100

    seller info loads into page

--- FrontEnd/myfirstapp/src/components/Seller/Seller.js
+++ FrontEnd/myfirstapp/src/components/Seller/Seller.js
@@ -8,60 +8,48 @@ export default (props) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.user);
     const location = useLocation()
-    const [loading, setLoading] = useState(true);
-    const [userList, setUserList] = useState([]);
     const [account, setAccount] = useState({});
-    const {seller} = location.state;
-
+    const { seller } = location.state;
 
     useEffect(() => {
-        console.log('seller',seller)
         dispatch(getUserAccountsList())
-        setLoading(false)
     }, [dispatch])
     useEffect(() => {
-        if (!loading) {
-            setUserList(user.userAccounts);
-            console.log('userlist',userList)
-        }
-    }, [loading])
-    useEffect(() => {
-        if (userList) {
-            userList.forEach(user => {
-                if (user.email == seller) {
+        if (!user.loading) {
+            user.userAccounts.forEach(user => {
+                if (user.fullName == seller) {
                     setAccount(user);
-                    console.log('acc',account)
                 }
             });
         }
-    }, [])
+    }, [user.loading, account])
+
     return (
         !account ? 'LOADING' :
-        <div style={{ display: 'flex' }}>
-            <Card style={{ width: '30%', margin: '5%' }}>
-                <h1>SELLER</h1>
-                <br /><br />
-                <h5>PENGUIN BOOKS</h5>
-                <h5>{account.email}</h5>
-                <h5>0123456789</h5>
-                <h5>0987654321123456</h5>
-            </Card>
+            <div style={{ display: 'flex' }}>
+                <Card style={{ width: '30%', margin: '5%' }}>
+                    <h1>{account.fullName}</h1>
+                    <br /><br />
+                    <h5>{account.username}</h5>
+                    <h5>{account.phoneNumber}</h5>
+                    <h5>{account.abn}</h5>
+                </Card>
 
-            <Card style={{ width: '50%', margin: '5%' }}>
-                <List
-                    itemLayout="horizontal"
-                    // dataSource={}
-                    renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                                title="USER'S NAME"
-                                description="REVIEW"
-                            />
-                        </List.Item>
-                    )}
-                />
+                <Card style={{ width: '50%', margin: '5%' }}>
+                    <List
+                        itemLayout="horizontal"
+                        // dataSource={}
+                        renderItem={item => (
+                            <List.Item>
+                                <List.Item.Meta
+                                    title="USER'S NAME"
+                                    description="REVIEW"
+                                />
+                            </List.Item>
+                        )}
+                    />
 
-            </Card>
-        </div>
+                </Card>
+            </div>
     );
 }
