c641cb60b82c720402cd7bc24769f8d4d283d1f8
s3742332 <s3742332@student.rmit.edu.au>
2021-10-17 13:20:51 +1100

    seller review microservice

---
+++ BackEnd/booksmicroservices/src/main/java/com/rmit/sept/bk_booksmicroservices/web/SellerReviewController.java
@@ -0,0 +1,27 @@
+package com.rmit.sept.bk_booksmicroservices.web;
+
+import com.rmit.sept.bk_booksmicroservices.Services.SellerReviewService;
+import com.rmit.sept.bk_booksmicroservices.model.SellerReview;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/sellerreviews")
+public class SellerReviewController
+{
+    @Autowired
+    private SellerReviewService sellerReviewService;
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/saveSellerReview")
+    public ResponseEntity<SellerReview> createNewSellerReview(@Valid @RequestBody SellerReview review)
+    {
+        SellerReview review1 = sellerReviewService.saveOrUpdateSellerReview(review);
+        return new ResponseEntity<SellerReview>(review1, HttpStatus.CREATED);
+    }
+}
